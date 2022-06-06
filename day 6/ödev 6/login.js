06179d9c3778a2a6393ec30c3de0a170e8142b6e
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-06-06 21:54:41 +0300

    ödev 6- login page

---
+++ day 6/ödev 6/login.js
@@ -0,0 +1,33 @@
+const dbUserEmail = "deneme@gmail.com";
+const dbUserPassword = "root";
+
+var btnLogin = document.getElementById("login");
+
+let attempts = 5;
+
+btnLogin.onclick = function () {
+  let userEmail = document.getElementById("userEmail").value;
+  let userPassword = document.getElementById("userPassword").value;
+
+  console.log(userEmail, userPassword);
+
+  // db ile karşılaştırma
+  if (attempts === 0) {
+    alert("Your account has been blocked.");
+  } else {
+    if (dbUserEmail === userEmail && dbUserPassword === userPassword) {
+      alert("Giriş yapıldı !");
+    } else {
+      attempts--;
+    }
+  }
+  if (attempts === 0) {
+    document.getElementById("attempts").innerHTML =
+      "Your account has been blocked.";
+  } else {
+    document.getElementById("attempts").innerHTML =
+      "You have " + attempts + " attempts left.";
+    document.getElementById("incorrect").innerHTML =
+      "Incorrect phone number or password";
+  }
+};
